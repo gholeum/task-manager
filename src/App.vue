ebc8eb6bb0c1628d2aa9f743e3b6bb553515ddd8
ROMAN <romzess007@mail.ru>
2024-08-12 20:35:17 +0300

    initialization

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <h1>Счётчик</h1>
+
+  <p>Счёт: <strong>{{ count }}</strong></p>
+
+  <button @click="increment">Действие <strong> + 1</strong></button>
+  <button @click="etobudetminus">Действие <strong> - 1</strong></button>
+</div>
+  <dogCard/>
+</template> 
+
+<script>
+import dogCard from './components/DogCard.vue'
+
+export default {
+  name: 'TheApp',
+
+  components: {
+    dogCard,
+  },
+
+  data() {
+    return {
+        count: 0
+    }
+  },
+
+  methods: {
+    increment() {
+      this.count += 1;
+    },
+    etobudetminus() {
+      this.count -= 1;
+    },
+  },
+}
+</script>
+
+<style>
+div {
+  align-items: center;
+  text-align: center;
+}
+button {
+  background-color: red;
+  color: white;
+  width: 250px;
+  height: auto;
+  border-radius: 0px 30px;
+  border: 2px dashed black;
+  font-size: 20pt
+}
+p {
+  font-size: 36pt;
+  color: red
+}
+  </style>
---
+++ src/components/DogCard.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <img src="https://kartinki.pics/pics/uploads/posts/2022-09/1663621633_1-kartinkin-net-p-grustnaya-sobachka-vkontakte-1.jpg" alt="фотография">
+        <h1>Это {{ dogName }}</h1>
+        <h3><strong>Возраст:</strong> {{ dogAge }}</h3>
+        <p>живёт в конуре ({{dogHome}})</p>
+        <button @click="changeDog">хочу настоящую сабачку</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'dogCard',
+
+  data() {
+    return {
+        dogName: "собака",
+        dogAge: "никто не знает(((",
+        dogHome: "наверное"
+    }
+  },
+
+  methods: {
+    changeDog() {
+      this.dogName = "настоящая сабачка урааа";
+      this.dogAge = "стоп...";
+      this.dogHome = "точно"
+    }
+  },
+}
+</script>
+
+<style>
+div {
+  align-items: center;
+  text-align: center;
+}
+
+img {
+  width:20%;
+  height: auto
+}
+
+h1 {
+  color: #888;
+  font-size: 24pt
+}
+
+h3 {
+  color: #777
+}
+
+p {
+  color: #666
+}
+
+button {
+  background-color: black;
+  width: 200px; 
+  height: auto;
+  font-size: 18px;
+  color: #fff;
+  border-radius: 30px
+}
+</style>
